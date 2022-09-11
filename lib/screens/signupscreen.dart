06fc23567065d255f3b9c351520c9ebe7f748117
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-09-11 11:50:13 +0530

    pod updates

---
+++ lib/screens/signupscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class signUp extends StatelessWidget {
+
+  const signUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
